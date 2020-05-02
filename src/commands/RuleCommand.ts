e9365e985b8e18713eae3b3d5b7f54e9b2461650
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-05-02 14:00:54 +0100

    Added the ?rule command.

---
+++ src/commands/RuleCommand.ts
@@ -0,0 +1,38 @@
+import { Message, MessageEmbed } from "discord.js";
+import { rules } from "../config.json";
+import Command from "../abstracts/Command";
+import RunnableCommand from "../interfaces/RunnableCommand";
+
+class RuleCommand extends Command implements RunnableCommand {
+	constructor() {
+		super(
+			"rule",
+			"Get a specific rule."
+		);
+	}
+
+	async run(message: Message, args: string[]): Promise<void> {
+		const embed = new MessageEmbed();
+
+		if (typeof args[0] === "undefined") {
+			embed.setTitle("Error");
+			embed.setDescription("You must define a rule number.");
+			embed.addField("Correct Usage", "?rule <rule number/trigger>");
+		} else {
+			const rule = rules.find(rule => rule.triggers.includes(args[0]));
+
+			if (rule !== undefined) {
+				embed.setTitle(rule.name);
+				embed.setDescription(rule.description);
+			} else {
+				embed.setTitle("Error");
+				embed.setDescription("Unknown rule number/trigger.");
+				embed.addField("Correct Usage", "?rule <rule number/trigger");
+			}
+		}
+
+		await message.channel.send({ embed });
+ 	}
+}
+
+export default RuleCommand;
