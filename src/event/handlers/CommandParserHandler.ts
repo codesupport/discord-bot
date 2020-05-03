b057ca78792b52d2c6804f2127c1ba1458a45a8f
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-05-03 18:19:26 +0100

    Moved factory creation into constructor.

--- src/event/handlers/CommandParserHandler.ts
+++ src/event/handlers/CommandParserHandler.ts
@@ -4,21 +4,24 @@ import EventHandler from "../../abstracts/EventHandler";
 import CommandFactory from "../../factories/CommandFactory";
 
 class CommandParserHandler extends EventHandler {
+	private readonly commandFactory: CommandFactory;
+
 	constructor() {
 		super(Constants.Events.MESSAGE_CREATE);
+
+		this.commandFactory = new CommandFactory();
+		this.commandFactory.loadCommands();
+
+		this.handle = this.handle.bind(this);
 	}
 
 	async handle(message: Message): Promise<void> {
-		const commandFactory = new CommandFactory();
-
-		await commandFactory.loadCommands();
-
 		if (message.content.startsWith(COMMAND_PREFIX)) {
 			const args = message.content.replace("?", "").split(" ");
 			const trigger = args.shift() || args[0];
 
-			if (commandFactory.commandExists(trigger)) {
-				await commandFactory.getCommand(trigger).run(message, args);
+			if (this.commandFactory.commandExists(trigger)) {
+				await this.commandFactory.getCommand(trigger).run(message, args);
 			}
 		}
 	}
