6498352ce7ff9478b73e3025a60d8e29e6f2ee71
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-05-03 15:15:06 +0100

    Moved getting files in a directory logic out of CommandFactory into getFilesInDirectory

--- src/factories/CommandFactory.ts
+++ src/factories/CommandFactory.ts
@@ -1,15 +1,15 @@
 import { commands_directory } from "../config.json";
-import { readDirectory } from "../utils/readDirectory";
 import Command from "../abstracts/Command";
+import getFilesInDirectory from "../utils/getFilesInDirectory";
 
 class CommandFactory {
 	private commands: any = {};
 
 	async loadCommands(): Promise<void> {
-		const commandsDirectory = await readDirectory(`${__dirname}/../${commands_directory}`);
-		const commandFiles = commandsDirectory
-			.filter(file => file.endsWith("Command.js"))
-			.map(file => require(`${__dirname}/../${commands_directory}/${file}`));
+		const commandFiles = await getFilesInDirectory(
+			`${__dirname}/../${commands_directory}`,
+			"Command.js"
+		);
 
 		commandFiles.forEach((command) => {
 			const { default: Command } = command;
