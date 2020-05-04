ab8ff7875ebb8594f6b0310dd170e06f289b956f
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-05-04 13:04:33 +0100

    Automatically give Member role if the user seems valid.

---
+++ src/event/handlers/AutomaticMemberRoleHandler.ts
@@ -0,0 +1,23 @@
+import { Constants, GuildMember } from "discord.js";
+import { MEMBER_ROLE } from "../../config.json";
+import EventHandler from "../../abstracts/EventHandler";
+
+class AutomaticMemberRoleHandler extends EventHandler {
+	constructor() {
+		super(Constants.Events.GUILD_MEMBER_ADD);
+	}
+
+	async handle(member: GuildMember): Promise<void> {
+		const currentDate = Date.now();
+		const memberCreatedDate = member.user.createdAt.getTime();
+		const dateDifference = currentDate - memberCreatedDate;
+		const hasAvatar = member.user.avatar;
+		const is30DaysOld = dateDifference / 2592000000 > 1;
+
+		if (hasAvatar && is30DaysOld) {
+			await member.roles.add(MEMBER_ROLE, "Appears to be a valid account.");
+		}
+	}
+}
+
+export default AutomaticMemberRoleHandler;
