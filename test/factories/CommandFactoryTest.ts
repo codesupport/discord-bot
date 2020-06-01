e47cf4c40e70d8afeb7bfffbc2a568d4c76358a0
Allan <allanjuansil@gmail.com>
2020-06-01 10:13:58 -0300

    Test alias behaviour on CommandFactoryTest

--- test/factories/CommandFactoryTest.ts
+++ test/factories/CommandFactoryTest.ts
@@ -3,17 +3,28 @@ import { expect } from "chai";
 import CommandFactory from "../../src/factories/CommandFactory";
 // @ts-ignore - TS does not like MockCommand not living in src/
 import MockCommand from "../MockCommand";
+import MockCommandWithAlias from "../MockCommandWithAlias";
 
 describe("CommandFactory", () => {
     let factory: CommandFactory;
     let commandName: string;
+	let commandWithAliasesName: string;
+	let aliases: string[];
 
     before(() => {
-        factory = new CommandFactory();
         commandName = new MockCommand().getName();
+		commandWithAliasesName = new MockCommandWithAlias().getName();
+		aliases = new MockCommandWithAlias().getAliases();
+
+		factory = new CommandFactory();
         factory["commands"] = {};
         factory["commands"][commandName] = () => new MockCommand();
-    });
+		factory["commands"][commandWithAliasesName] = () => new MockCommandWithAlias();
+
+		aliases.forEach(alias => {
+			factory["commands"][alias] = () => new MockCommandWithAlias();
+		});
+	});
 
     describe("commandExists()", () => {
         it("checks to see a command exists", () => {
@@ -30,7 +41,13 @@ describe("CommandFactory", () => {
 
         it("checks to see a command doesn't exist", () => {
             expect(factory.commandExists("bad command")).to.be.false;
-        });
+		});
+		
+		it("checks to see a command exists by it's aliases", () => {
+			aliases.forEach(alias => {
+				expect(factory.commandExists(alias)).to.be.true;
+			});
+		});
     });
 
     describe("getCommand()", () => {
@@ -44,6 +61,12 @@ describe("CommandFactory", () => {
 
         it("gets a mocked command - uppercase", () => {
             expect(factory.getCommand(commandName.toUpperCase())).to.be.a("object");
-        });
+		});
+		
+		it("gets a mocked command by it's aliases", () => {
+			aliases.forEach(alias => {
+				expect(factory.getCommand(alias)).to.be.a("object");
+			});
+		});
     });
 });
