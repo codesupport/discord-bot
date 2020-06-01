4c23b6a166a94dd0fcca146a18b6f71edaa4892c
Allan <allanjuansil@gmail.com>
2020-06-01 10:19:33 -0300

    Fix indentation problems

--- test/factories/CommandFactoryTest.ts
+++ test/factories/CommandFactoryTest.ts
@@ -6,19 +6,19 @@ import MockCommand from "../MockCommand";
 import MockCommandWithAlias from "../MockCommandWithAlias";
 
 describe("CommandFactory", () => {
-    let factory: CommandFactory;
-    let commandName: string;
+	let factory: CommandFactory;
+	let commandName: string;
 	let commandWithAliasesName: string;
 	let aliases: string[];
 
-    before(() => {
-        commandName = new MockCommand().getName();
+	before(() => {
+		commandName = new MockCommand().getName();
 		commandWithAliasesName = new MockCommandWithAlias().getName();
 		aliases = new MockCommandWithAlias().getAliases();
 
 		factory = new CommandFactory();
-        factory["commands"] = {};
-        factory["commands"][commandName] = () => new MockCommand();
+		factory["commands"] = {};
+		factory["commands"][commandName] = () => new MockCommand();
 		factory["commands"][commandWithAliasesName] = () => new MockCommandWithAlias();
 
 		aliases.forEach(alias => {
@@ -26,47 +26,47 @@ describe("CommandFactory", () => {
 		});
 	});
 
-    describe("commandExists()", () => {
-        it("checks to see a command exists", () => {
-            expect(factory.commandExists(commandName)).to.be.true;
-        });
-
-        it("checks to see a command exists - lowercase", () => {
-            expect(factory.commandExists(commandName.toLowerCase())).to.be.true;
-        });
-
-        it("checks to see a command exists - uppercase", () => {
-            expect(factory.commandExists(commandName.toUpperCase())).to.be.true;
-        });
-
-        it("checks to see a command doesn't exist", () => {
-            expect(factory.commandExists("bad command")).to.be.false;
+	describe("commandExists()", () => {
+		it("checks to see a command exists", () => {
+			expect(factory.commandExists(commandName)).to.be.true;
 		});
-		
+
+		it("checks to see a command exists - lowercase", () => {
+			expect(factory.commandExists(commandName.toLowerCase())).to.be.true;
+		});
+
+		it("checks to see a command exists - uppercase", () => {
+			expect(factory.commandExists(commandName.toUpperCase())).to.be.true;
+		});
+
+		it("checks to see a command doesn't exist", () => {
+			expect(factory.commandExists("bad command")).to.be.false;
+		});
+
 		it("checks to see a command exists by it's aliases", () => {
 			aliases.forEach(alias => {
 				expect(factory.commandExists(alias)).to.be.true;
 			});
 		});
-    });
+	});
 
-    describe("getCommand()", () => {
-        it("gets a mocked command", () => {
-            expect(factory.getCommand(commandName)).to.be.a("object");
-        });
-
-        it("gets a mocked command - lowercase", () => {
-            expect(factory.getCommand(commandName.toLowerCase())).to.be.a("object");
-        });
-
-        it("gets a mocked command - uppercase", () => {
-            expect(factory.getCommand(commandName.toUpperCase())).to.be.a("object");
+	describe("getCommand()", () => {
+		it("gets a mocked command", () => {
+			expect(factory.getCommand(commandName)).to.be.a("object");
 		});
-		
+
+		it("gets a mocked command - lowercase", () => {
+			expect(factory.getCommand(commandName.toLowerCase())).to.be.a("object");
+		});
+
+		it("gets a mocked command - uppercase", () => {
+			expect(factory.getCommand(commandName.toUpperCase())).to.be.a("object");
+		});
+
 		it("gets a mocked command by it's aliases", () => {
 			aliases.forEach(alias => {
 				expect(factory.getCommand(alias)).to.be.a("object");
 			});
 		});
-    });
+	});
 });
