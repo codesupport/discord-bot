094beaadb71891f89815a199b4d2a8da52cefbe1
Jacob Gasser <me@jacobgasser.com>
2020-06-09 18:38:57 -0400

    Formatted files

--- src/event/handlers/LogMessageDeleteHandler.ts
+++ src/event/handlers/LogMessageDeleteHandler.ts
@@ -3,21 +3,21 @@ import { LOG_CHANNEL_ID } from "../../config.json";
 import EventHandler from "../../abstracts/EventHandler";
 
 class LogMessageUpdateHandler extends EventHandler {
-    constructor() {
-        super(Constants.Events.MESSAGE_DELETE);
-    }
+	constructor() {
+		super(Constants.Events.MESSAGE_DELETE);
+	}
 
-    async handle(message: Message): Promise<void> {
-        const embed = new MessageEmbed();
+	async handle(message: Message): Promise<void> {
+		const embed = new MessageEmbed();
 
-        embed.setTitle("Message Deleted");
-        embed.setDescription(`Author: <@${message.author}>\nChannel: <#${message.channel}>`);
-        embed.addField("Message", message.content);
+		embed.setTitle("Message Deleted");
+		embed.setDescription(`Author: <@${message.author}>\nChannel: <#${message.channel}>`);
+		embed.addField("Message", message.content);
 
-        const logsChannel = message.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
+		const logsChannel = message.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
 
-        await logsChannel?.send({ embed });
-    }
+		await logsChannel?.send({ embed });
+	}
 }
 
 export default LogMessageUpdateHandler;
--- src/event/handlers/LogMessageUpdateHandler.ts
+++ src/event/handlers/LogMessageUpdateHandler.ts
@@ -3,30 +3,30 @@ import { LOG_CHANNEL_ID } from "../../config.json";
 import EventHandler from "../../abstracts/EventHandler";
 
 class LogMessageUpdateHandler extends EventHandler {
-    constructor() {
-        super(Constants.Events.MESSAGE_UPDATE);
-    }
+	constructor() {
+		super(Constants.Events.MESSAGE_UPDATE);
+	}
 
-    async handle(oldMessage: Message, newMessage: Message): Promise<void> {
-        if (oldMessage.content === newMessage.content) {
-            return;
-        }
+	async handle(oldMessage: Message, newMessage: Message): Promise<void> {
+		if (oldMessage.content === newMessage.content) {
+			return;
+		}
 
-        if (newMessage.content === "") {
-            return;
-        }
+		if (newMessage.content === "") {
+			return;
+		}
 
-        const embed = new MessageEmbed();
+		const embed = new MessageEmbed();
 
-        embed.setTitle("Message Updated");
-        embed.setDescription(`Author: <@${oldMessage.author}>\nChannel: <#${oldMessage.channel}>`);
-        embed.addField("Old Message", oldMessage.content);
-        embed.addField("New Message", newMessage.content);
+		embed.setTitle("Message Updated");
+		embed.setDescription(`Author: <@${oldMessage.author}>\nChannel: <#${oldMessage.channel}>`);
+		embed.addField("Old Message", oldMessage.content);
+		embed.addField("New Message", newMessage.content);
 
-        const logsChannel = oldMessage.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
+		const logsChannel = oldMessage.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
 
-        await logsChannel?.send({ embed });
-    }
+		await logsChannel?.send({ embed });
+	}
 }
 
 export default LogMessageUpdateHandler;
