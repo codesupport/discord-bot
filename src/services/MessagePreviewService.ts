6ce991bfdcbf5014f9749d4f1867e532af9862a0
Sara Mae <34497797+saramaebee@users.noreply.github.com>
2020-06-22 09:16:58 -0400

    quote handler

---
+++ src/services/MessagePreviewService.ts
@@ -0,0 +1,46 @@
+import { Message, TextChannel, MessageEmbed } from "discord.js";
+
+class MessagePreviewService {
+	private static instance: MessagePreviewService;
+
+	/* eslint-disable */
+	private constructor() {}
+	/* eslint-enable */
+
+	static getInstance(): MessagePreviewService {
+		if (!this.instance) {
+			this.instance = new MessagePreviewService();
+		}
+
+		return this.instance;
+	}
+
+	async generatePreview(link: string, callingMessage: Message): Promise<void> {
+		const msgArray = this.stripLink(link);
+
+		if (this.verifyGuild(callingMessage, msgArray[0])) {
+			if (callingMessage.guild?.available) {
+				const channel = callingMessage.guild.channels.cache.get(msgArray[1]) as TextChannel;
+				const messageToPreview = await channel.messages.fetch(msgArray[2]);
+
+				const embed = new MessageEmbed()
+					.setAuthor(messageToPreview.member?.nickname || messageToPreview.author.username, messageToPreview.author.avatarURL() || undefined, link)
+					.addField(`Called by ${callingMessage.member?.nickname || callingMessage.author.username}`, `[Click for context](${link})`)
+					.setDescription(`${messageToPreview.content}\n`)
+					.setColor(messageToPreview.member?.displayColor || "#FFFFFE");
+
+				callingMessage.channel.send(embed);
+			}
+		}
+	}
+
+	verifyGuild(message: Message, guildId: string): boolean {
+		return guildId === message.guild?.id;
+	}
+
+	stripLink(link: string): string[] {
+		return link.substring(29).split("/");
+	}
+}
+
+export default MessagePreviewService;
