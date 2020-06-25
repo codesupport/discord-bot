36c26078eabd3482506fc9740e5beba164bae027
Banana Crazy <grappig.13@hotmail.com>
2020-06-25 15:50:32 +0200

    Not logging a deleted message if content is empty

--- src/event/handlers/LogMessageDeleteHandler.ts
+++ src/event/handlers/LogMessageDeleteHandler.ts
@@ -8,16 +8,18 @@ class LogMessageDeleteHandler extends EventHandler {
 	}
 
 	async handle(message: Message): Promise<void> {
-		const embed = new MessageEmbed();
+		if (message.content !== "") {
+			const embed = new MessageEmbed();
 
-		embed.setTitle("Message Deleted");
-		embed.setDescription(`Author: <@${message.author}>\nChannel: <#${message.channel}>`);
-		embed.addField("Message", message.content);
-		embed.setColor(EMBED_COLOURS.DEFAULT);
+			embed.setTitle("Message Deleted");
+			embed.setDescription(`Author: <@${message.author}>\nChannel: <#${message.channel}>`);
+			embed.addField("Message", message.content);
+			embed.setColor(EMBED_COLOURS.DEFAULT);
 
-		const logsChannel = message.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
+			const logsChannel = message.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
 
-		await logsChannel?.send({ embed });
+			await logsChannel?.send({ embed });
+		}
 	}
 }
 
