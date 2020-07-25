947ff8686cf121942d3ac832d4d743f1d93c4319
Allan <allanjuansil@gmail.com>
2020-07-25 20:20:27 -0300

    Remove unnecessary stuff

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Client, TextChannel } from "discord.js";
 import { config as env } from "dotenv";
 import DirectoryUtils from "./utils/DirectoryUtils";
-import { handlers_directory, AUTHENTICATION_MESSAGE_CHANNEL, AUTHENTICATION_MESSAGE_ID, PRODUCTION_ENV, GENERAL_CHANNEL_ID } from "./config.json";
+import { handlers_directory, AUTHENTICATION_MESSAGE_CHANNEL, AUTHENTICATION_MESSAGE_ID, PRODUCTION_ENV } from "./config.json";
 
 const client = new Client();
 
--- test/utils/DirectoryUtilsTest.ts
+++ test/utils/DirectoryUtilsTest.ts
@@ -2,22 +2,8 @@ import fs, { Dirent } from "fs";
 import { expect } from "chai";
 import { createSandbox, SinonSandbox } from "sinon";
 import DirectoryUtils from "../../src/utils/DirectoryUtils";
-import { commands_directory } from "../../src/config.json";
-import { promisify } from "util";
 
 describe("DirectoryUtils", () => {
-	describe("::readDirectory()", () => {
-		let sandbox: SinonSandbox;
-
-		beforeEach(() => {
-			sandbox = createSandbox();
-		});
-
-		afterEach(() => {
-			sandbox.restore();
-		});
-	});
-
 	describe("::getFilesInDirectory()", () => {
 		let sandbox: SinonSandbox;
 
