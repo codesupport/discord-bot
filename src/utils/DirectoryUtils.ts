29a7d0733b412bc154c3fb3f8851257929521559
Allan <allanjuansil@gmail.com>
2020-07-25 20:20:35 -0300

    Try to fix indentation

--- src/utils/DirectoryUtils.ts
+++ src/utils/DirectoryUtils.ts
@@ -3,12 +3,13 @@ import { promisify } from "util";
 
 class DirectoryUtils {
     static readDirectory = promisify(readdir);
+
     private static require = require;
 
     static async getFilesInDirectory(directory: string, ending: string): Promise<any[]> {
-	    const directoryContents = await this.readDirectory(directory);
+        const directoryContents = await this.readDirectory(directory);
 
-	    /* eslint-disable */
+        /* eslint-disable */
         return directoryContents
             .filter(file => file.endsWith(ending))
             .map(file => this.require(`${directory}/${file}`));
