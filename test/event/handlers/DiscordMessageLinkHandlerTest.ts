53bdf736ca70e2f692f4c49c1be9f6203dc737c0
Sara Mae <34497797+saramaebee@users.noreply.github.com>
2020-07-25 16:51:54 -0400

    adding test

---
+++ test/event/handlers/DiscordMessageLinkHandlerTest.ts
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import { Constants } from "discord.js";
+import DiscordMessageLinkHandler from "../../../src/event/handlers/DiscordMessageLinkHandler";
+import { SinonSandbox, createSandbox } from "sinon";
+import EventHandler from "../../../src/abstracts/EventHandler";
+import MockDiscord from "../../MockDiscord";
+
+describe("DiscordMessageLinkHandler", () => {
+	describe("Constructor()", () => {
+		it("creates a handler for MESSAGE_Create", () => {
+			const handler = new DiscordMessageLinkHandler();
+
+			expect(handler.getEvent()).to.equal(Constants.Events.MESSAGE_CREATE);
+		});
+	});
+})
