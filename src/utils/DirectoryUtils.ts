f36bfc9610b645d9b3423e1da8bfdb455afbaec3
Allan <allanjuansil@gmail.com>
2020-07-25 20:48:10 -0300

    Disable eslint

--- src/utils/DirectoryUtils.ts
+++ src/utils/DirectoryUtils.ts
@@ -6,15 +6,15 @@ class DirectoryUtils {
 
     private static require = require;
 
+    /* eslint-disable */
     static async getFilesInDirectory(directory: string, ending: string): Promise<any[]> {
         const directoryContents = await this.readDirectory(directory);
 
-        /* eslint-disable */
         return directoryContents
             .filter(file => file.endsWith(ending))
             .map(file => this.require(`${directory}/${file}`));
-        /* eslint-enable */
     }
+    /* eslint-enable */
 }
 
 export default DirectoryUtils;
