914ddb830542d58e7db346bed915953ae8180e32
Allan <allanjuansil@gmail.com>
2020-07-29 22:53:59 -0300

    Improve test description

--- test/utils/getEnvironmentVariableTest.ts
+++ test/utils/getEnvironmentVariableTest.ts
@@ -2,12 +2,12 @@ import { expect } from "chai";
 import getEnvironmentVariable from "../../src/utils/getEnvironmentVariable";
 
 describe("getEnvironmentVariable", () => {
-	it("should return variable if it is truthy", () => {
+	it("should return variable if it is set", () => {
 		process.env.FAKE_VAR = "fake value";
 		expect(getEnvironmentVariable("FAKE_VAR")).to.equal("fake value");
 	});
 
-	it("should throw error if variable is falsy", () => {
+	it("should throw error if variable is not set", () => {
 		expect(() => getEnvironmentVariable("FAKE_VAR")).to.throw('The environment variable "FAKE_VAR" is not set.');
 	});
 
