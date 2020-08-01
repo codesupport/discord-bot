7857179fd7ea8d4f59c3049742e36f244062f0ea
Allan <allanjuansil@gmail.com>
2020-07-31 22:28:19 -0300

    Add test for auth channel in production

--- test/app/appTest.ts
+++ test/app/appTest.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import { SinonSandbox, createSandbox, SinonStub } from "sinon";
-import { Client } from "discord.js";
+import { Client, TextChannel, ChannelManager } from "discord.js";
 
 import app from "../../src/app";
 import DirectoryUtils from "../../src/utils/DirectoryUtils";
 import MockHandler from "../MockHandler";
+import MockDiscord from "../MockDiscord";
+import { handlers_directory, AUTHENTICATION_MESSAGE_CHANNEL, AUTHENTICATION_MESSAGE_ID, PRODUCTION_ENV } from "../../src/config.json";
 
 describe("app", () => {
 	let sandbox: SinonSandbox;
@@ -47,6 +49,25 @@ describe("app", () => {
 		await app();
 
 		expect(onStub.calledWith(handler.getEvent(), handler.handle));
+	});
+
+	it("should fetch auth channel and messages in production environment", async () => {
+		const testEnv = process.env.NODE_ENV;
+
+		process.env.NODE_ENV = PRODUCTION_ENV;
+
+		const mockDiscord = new MockDiscord();
+		const textChannel = mockDiscord.getTextChannel();
+
+		const fetchChannelsStub = sandbox.stub(ChannelManager.prototype, "fetch").callsFake(async () => textChannel);
+		const fetchMessagesStub = sandbox.stub(textChannel.messages, "fetch");
+
+		await app();
+
+		expect(fetchChannelsStub.calledWith(AUTHENTICATION_MESSAGE_CHANNEL)).to.be.true;
+		expect(fetchMessagesStub.calledWith(AUTHENTICATION_MESSAGE_ID)).to.be.true;
+
+		process.env.NODE_ENV = testEnv;
 	});
 
 	afterEach(() => {
