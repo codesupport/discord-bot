6e69fc60bf27a583bb146b5c17c176b4a463efea
Allan <allanjuansil@gmail.com>
2020-08-15 16:43:40 -0300

    Fix formatting

--- src/commands/ResourcesCommand.ts
+++ src/commands/ResourcesCommand.ts
@@ -3,16 +3,18 @@ import Command from "../abstracts/Command";
 
 class ResourcesCommand extends Command {
 	constructor() {
-		super("resources",
+		super(
+			"resources",
 			"Displays a link to the resources page of CodeSupport's website.",
 			{
 				aliases: ["resource"]
-			});
+			}
+		);
 	}
 
 	async run(message: Message, args: string[]) {
 		let url = "https://codesupport.dev/resources";
-		const category = args[0];
+		const [category] = args;
 
 		if (category) {
 			url += `?category=${category}`;
