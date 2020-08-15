d10344a310e997dd39751056c1542d8be0a1925e
Allan <allanjuansil@gmail.com>
2020-08-15 15:20:43 -0300

    Create ResourcesCommand

---
+++ src/commands/ResourcesCommand.ts
@@ -0,0 +1,25 @@
+import { Message } from "discord.js";
+import Command from "../abstracts/Command";
+
+class ResourcesCommand extends Command {
+	constructor() {
+		super("resources",
+			"Displays a link to the resources page of CodeSupport's website.",
+			{
+				aliases: ["resource"]
+			});
+	}
+
+	async run(message: Message, args: string[]) {
+		let url = "https://codesupport.dev/resources";
+		const category = args[0];
+
+		if (category) {
+			url += `?category=${category}`;
+		}
+
+		await message.channel.send(url);
+	}
+}
+
+export default ResourcesCommand;
