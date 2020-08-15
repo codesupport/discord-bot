bce82acfa139e361409bda038e33a98989520f4b
Allan <allanjuansil@gmail.com>
2020-08-15 15:41:33 -0300

    Test ResourcesCommand run method

--- test/commands/ResourcesCommandTest.ts
+++ test/commands/ResourcesCommandTest.ts
@@ -1,5 +1,9 @@
 import { expect } from "chai";
+import { SinonSandbox, createSandbox } from "sinon";
 import ResourcesCommand from "../../src/commands/ResourcesCommand";
+import { Message } from "discord.js";
+import Command from "../../src/abstracts/Command";
+import MockDiscord from "../MockDiscord";
 
 describe("ResourcesCommand", () => {
 	describe("constructor()", () => {
@@ -19,6 +23,50 @@ describe("ResourcesCommand", () => {
 			const command = new ResourcesCommand();
 
 			expect(command.getAliases().includes("resource")).to.be.true;
+		});
+	});
+
+	describe("run()", () => {
+		let sandbox: SinonSandbox;
+		let message: Message;
+		let command: Command;
+		let discordMock: MockDiscord;
+
+		beforeEach(() => {
+			sandbox = createSandbox();
+			command = new ResourcesCommand();
+			discordMock = new MockDiscord();
+			message = discordMock.getMessage();
+		});
+
+		it("sends a message to the channel", async () => {
+			const messageMock = sandbox.stub(message.channel, "send");
+
+			await command.run(message, []);
+
+			expect(messageMock.calledOnce).to.be.true;
+		});
+
+		it("sends the link to resources page if no argument is given", async () => {
+			const messageMock = sandbox.stub(message.channel, "send");
+
+			await command.run(message, []);
+
+			const url = messageMock.firstCall.lastArg;
+
+			expect(messageMock.calledOnce).to.be.true;
+			expect(url).to.equal("https://codesupport.dev/resources");
+		});
+
+		it("sends link to the category page if an argument is given", async () => {
+			const messageMock = sandbox.stub(message.channel, "send");
+
+			await command.run(message, ["javascript"]);
+
+			const url = messageMock.firstCall.lastArg;
+
+			expect(messageMock.calledOnce).to.be.true;
+			expect(url).to.equal("https://codesupport.dev/resources?category=javascript");
 		});
 	});
 });
