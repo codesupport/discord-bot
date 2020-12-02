2ff898a0d62e86f0aa0c6e03c38ef8fabd0549b6
Jonathan <jonajvdm@gmail.com>
2020-12-02 14:31:47 +0100

    Added adventofcode command to see the leaderboard

---
+++ src/commands/AdventofcodeCommand.ts
@@ -0,0 +1,81 @@
+import { Message, MessageEmbed } from "discord.js";
+import Command from "../abstracts/Command";
+import AdventOfCodeService from "../services/AdventOfCodeService";
+import { EMBED_COLOURS, ADVENTOFCODE_LEADERBOARD, ADVENTOFCODE_YEAR, ADVENTOFCODE_INVITE } from "../config.json";
+
+class AdventofcodeCommand extends Command {
+	constructor() {
+		super(
+			"Adventofcode",
+			"Shows the current leaderboard for adventofcode.",
+			{
+				aliases: ["aoc"]
+			},
+		);
+	}
+
+	async run(message: Message): Promise<void> {
+		const amountInTopList = 15;
+		const link = `https://adventofcode.com/${ADVENTOFCODE_YEAR}/leaderboard/private/view/${ADVENTOFCODE_LEADERBOARD}`;
+
+		try {
+			const oac = AdventOfCodeService.getInstance();
+			const response = await oac.getLeaderBoard(ADVENTOFCODE_LEADERBOARD, ADVENTOFCODE_YEAR);
+
+			// Set members as a array for sorting
+			const members: AocMember[] = Object.values(response.members);
+
+			members.sort((a, b) => {
+				const stars = b.stars - a.stars;
+
+				return !stars ? b.local_score - a.local_score : stars;
+			});
+
+			// Get the max length sentence for members and stars
+			const memberNameLength = members.reduce((a, b) => {
+				const nameLengthA = a.name?.length || 0;
+				const nameLengthB = b.name?.length || 0;
+
+				return nameLengthA > nameLengthB ? a : b;
+			}).name?.length as number;
+
+			const starLength = members.reduce((a, b) => {
+				if (a.stars.toString().length > b.stars.toString().length) {
+					return a;
+				}
+				return b;
+			}).stars.toString().length;
+
+			let playerList = "```java\n(Name, Stars, Points) \n";
+
+			for (let i = 0; i < amountInTopList; i++) {
+				const member = members[i];
+
+				if (member) {
+					const name = !member.name ? member.id : member.name.padEnd(memberNameLength, " ");
+					const score = member.local_score;
+					const stars = member.stars.toString().padEnd(starLength);
+
+					playerList = playerList.concat(`${name} | ${stars} | ${score}\n`);
+				}
+			}
+
+			playerList = playerList.concat("```");
+
+			const description = `Leaderboard ID: \`${ADVENTOFCODE_INVITE}\`\n\n[View Leaderboard](${link})`;
+			const embed = new MessageEmbed();
+
+			embed.setTitle("Advent Of Code");
+			embed.setDescription(description);
+			embed.addField("Top 15", playerList);
+			embed.setColor(EMBED_COLOURS.SUCCESS);
+
+			message.channel.send(embed);
+		} catch (e) {
+			message.channel.send("Could not get the current leaderboard");
+			console.error("AOC went wrong", e);
+		}
+	}
+}
+
+export default AdventofcodeCommand;
---
+++ src/services/AdventOfCodeService.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosInstance } from "axios";
+import cache from "axios-cache-adapter";
+
+export default class AdventOfCodeService {
+	private static instance: AdventOfCodeService;
+	private api: AxiosInstance;
+
+	static getInstance(): AdventOfCodeService {
+		if (!this.instance) {
+			this.instance = new AdventOfCodeService();
+		}
+
+		return this.instance;
+	}
+
+	constructor() {
+		this.api = axios.create({
+			adapter: cache.setupCache({
+				maxAge: 15 * 60 * 1000
+			}).adapter
+		});
+	}
+
+	async getLeaderBoard(leaderBoard: string, year: number): Promise<AocLeaderBoard> {
+		const { ADVENT_OF_CODE_TOKEN } = process.env;
+		const link = `https://adventofcode.com/${year}/leaderboard/private/view/${leaderBoard}.json`;
+
+		const response = await this.api.get<AocLeaderBoard>(link, {
+			headers: {
+				"Cookie": `session=${ADVENT_OF_CODE_TOKEN};`
+			}
+		});
+
+		if (!response.data.members) {
+			throw Error("Advent Of code leaderboard not found");
+		}
+
+		return response.data;
+	}
+}
