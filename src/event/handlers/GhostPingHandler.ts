96cb60d78a04eecd26d42bf326f5106ad6b9572b
Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>
2020-12-05 23:34:31 +0100

    Update src/event/handlers/GhostPingHandler.ts
    Co-authored-by: Lambo <12111454+LamboCreeper@users.noreply.github.com>

--- src/event/handlers/GhostPingHandler.ts
+++ src/event/handlers/GhostPingHandler.ts
@@ -10,7 +10,10 @@ class GhostPingHandler extends EventHandler {
 	async handle(message: Message): Promise<void> {
 		if (message.mentions.users.first() || message.mentions.roles.first()) {
 			if (!message.author?.bot) {
-				if (message.mentions.users.first()?.id === message.author.id && message.mentions.users.size === 1) return;
+				const usersMentioned = message.mentions.users;
+				
+				if (usersMentioned.first()?.id === message.author.id && usersMentioned.size === 1) return;
+
 				const embed = new MessageEmbed();
 
 				embed.setTitle("Ghost Ping Detected!");
