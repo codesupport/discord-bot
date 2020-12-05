32b0782031deb23fb83d7a7902986d6259fe3cfa
BlackBearFTW <robinmager@home.nl>
2020-12-06 00:08:31 +0100

    Inspect Command
    - Now allows user id arguments
    - Arguments are now optional, will self inspect without argument.
    - Role list wont contain everyone role
    - "Join at" became "Joined at"
    - Now Throws error when userObj is undefined
    - Updated error message
    - Changed scope of userObj so it can be used in multiple if statements

--- src/commands/InspectCommand.ts
+++ src/commands/InspectCommand.ts
@@ -14,20 +14,13 @@ class InspectCommand extends Command {
 	async run(message: Message, args: string[]) {
 		const embed = new MessageEmbed();
 
-		const hasNoArgs = !args || typeof args[0] === "undefined";
-		const notCorrectFormat = !args[0]?.includes("#");
+		try {
+			let userObj;
 
-		if (hasNoArgs || notCorrectFormat) {
-			embed.setTitle("Error");
-			embed.setDescription("You must provide a username and discriminator");
-			embed.addField("Correct Usage", "?inspect <username>");
-			embed.setColor(EMBED_COLOURS.ERROR);
-		} else {
-			try {
+			if (args.length > 0 && isNaN(Number(args[0]))) {
 				const userData = args[0].split("#");
 				const username = userData[0];
 				const discriminator = userData[1];
-				let userObj;
 
 				const userList = await message.guild?.members?.fetch({query: username, limit: 1000});
 
@@ -38,23 +31,31 @@ class InspectCommand extends Command {
 				} else if (userList.size === 1) {
 					userObj = userList?.first();
 				}
-
-				if (userObj === undefined) return;
-
-				embed.setTitle(`Inspecting ${userObj?.user.tag}`);
-				embed.setThumbnail(userObj?.user.displayAvatarURL());
-				embed.addField("User ID", userObj?.user.id);
-				embed.addField("Username", userObj?.user.tag);
-				embed.addField("Join At", DateUtils.formatAsText(userObj?.joinedAt));
-				if (userObj?.nickname !== null) embed.addField("Nickname", userObj?.nickname);
-				embed.addField("Roles", `${userObj.roles.cache.map(role => role.toString())}`);
-				embed.setColor(EMBED_COLOURS.SUCCESS);
-			} catch (error) {
-				embed.setTitle("Error");
-				embed.setDescription("There was a problem inspecting this user");
-				embed.addField("Correct Usage", "?inspect <username>");
-				embed.setColor(EMBED_COLOURS.ERROR);
+			} else if (args.length > 0) {
+				if (message.guild === null) return;
+				userObj = message.guild.members.cache.get(args[0]);
+			} else if (!args.length) {
+				if (message.member === null) return;
+				userObj = message.member;
 			}
+
+			if (userObj === undefined) throw "";
+			if (message.guild === undefined) return;
+			if (userObj?.joinedAt === null) return;
+
+			embed.setTitle(`Inspecting ${userObj?.user.tag}`);
+			embed.setThumbnail(userObj?.user.displayAvatarURL());
+			embed.addField("User ID", userObj?.user.id);
+			embed.addField("Username", userObj?.user.tag);
+			embed.addField("Joined At", DateUtils.formatAsText(userObj?.joinedAt));
+			if (userObj?.nickname !== null) embed.addField("Nickname", userObj?.nickname);
+			embed.addField("Roles", `${userObj.roles.cache.filter(role => role.id !== message?.guild!.id).map(role => ` ${role.toString()}`)}`);
+			embed.setColor(EMBED_COLOURS.SUCCESS);
+		} catch (error) {
+			embed.setTitle("Error");
+			embed.setDescription("Unable to inspect user");
+			embed.addField("Correct Usage", "?inspect [username + discriminator / userID]");
+			embed.setColor(EMBED_COLOURS.ERROR);
 		}
 
 		await message.channel.send({embed});
