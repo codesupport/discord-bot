520ed6571131ff65aa7f73357f87159600beeef8
BlackBearFTW <robinmager@home.nl>
2020-12-06 23:29:22 +0100

    Made argument more secure
    - Now checks if argument only contains digits, if so its a user id
    - If not, checks if it matches regex for username + tag.
    - User id now uses fetch instead of cache
    - Incorrect argument & Unknown user are now two separate errors

--- src/commands/InspectCommand.ts
+++ src/commands/InspectCommand.ts
@@ -16,44 +16,54 @@ class InspectCommand extends Command {
 
 		try {
 			let userObj;
+			let userList;
 
-			if (args.length > 0 && isNaN(Number(args[0]))) {
-				const userData = args[0].split("#");
-				const username = userData[0];
-				const discriminator = userData[1];
+			if (args.length > 0) {
+				if ((/^\d+$/g).test(args[0])) {
+					userObj = await message.guild?.members?.fetch(args[0]);
+				} else {
+					if (!(/^.*#[0-9]{4}$/).test(args[0])) throw "";
 
-				const userList = await message.guild?.members?.fetch({query: username, limit: 1000});
+					const userData = args[0].split("#");
 
-				if (userList === undefined) return;
+					const username = userData[0];
+					const discriminator = userData[1];
 
-				if (userList?.size > 1) {
-					userObj = await userList?.find(memberObject => memberObject.user.discriminator === discriminator);
-				} else if (userList.size === 1) {
-					userObj = userList?.first();
+					userList = await message.guild?.members?.fetch({query: username, limit: 1000});
+
+					if (userList === undefined) return;
+
+					if (userList?.size > 1) {
+						userObj = await userList?.find(memberObject => memberObject.user.discriminator === discriminator);
+					} else if (userList.size === 1) {
+						userObj = userList?.first();
+					}
 				}
-			} else if (args.length > 0) {
-				if (message.guild === null) return;
-				userObj = message.guild.members.cache.get(args[0]);
-			} else if (!args.length) {
+			} else {
 				if (message.member === null) return;
 				userObj = message.member;
 			}
 
-			if (userObj === undefined) throw "";
-			if (message.guild === undefined) return;
-			if (userObj?.joinedAt === null) return;
+			if (userObj !== undefined) {
+				if (message.guild === undefined) return;
+				if (userObj?.joinedAt === null) return;
 
-			embed.setTitle(`Inspecting ${userObj?.user.tag}`);
-			embed.setThumbnail(userObj?.user.displayAvatarURL());
-			embed.addField("User ID", userObj?.user.id);
-			embed.addField("Username", userObj?.user.tag);
-			embed.addField("Joined At", DateUtils.formatAsText(userObj?.joinedAt));
-			if (userObj?.nickname !== null) embed.addField("Nickname", userObj?.nickname);
-			embed.addField("Roles", `${userObj.roles.cache.filter(role => role.id !== message?.guild!.id).map(role => ` ${role.toString()}`)}`);
-			embed.setColor(EMBED_COLOURS.SUCCESS);
+				embed.setTitle(`Inspecting ${userObj?.user.tag}`);
+				embed.setThumbnail(userObj?.user.displayAvatarURL());
+				embed.addField("User ID", userObj?.user.id);
+				embed.addField("Username", userObj?.user.tag);
+				embed.addField("Joined At", DateUtils.formatAsText(userObj?.joinedAt));
+				if (userObj?.nickname !== null) embed.addField("Nickname", userObj?.nickname);
+				embed.addField("Roles", `${userObj.roles.cache.filter(role => role.id !== message?.guild!.id).map(role => ` ${role.toString()}`)}`);
+				embed.setColor(EMBED_COLOURS.SUCCESS);
+			} else {
+				embed.setTitle("Error");
+				embed.setDescription("No user was found with this username/userID.");
+				embed.setColor(EMBED_COLOURS.ERROR);
+			}
 		} catch (error) {
 			embed.setTitle("Error");
-			embed.setDescription("Unable to inspect user");
+			embed.setDescription("Incorrect usage of command");
 			embed.addField("Correct Usage", "?inspect [username + discriminator / userID]");
 			embed.setColor(EMBED_COLOURS.ERROR);
 		}
