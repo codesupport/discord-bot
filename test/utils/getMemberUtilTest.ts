57b6d8dc0c4bd3d030476dc1fd0e5782073505a8
BlackBearFTW <robinmager@home.nl>
2021-01-08 20:52:53 +0100

    writing test for util

---
+++ test/utils/getMemberUtilTest.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { createSandbox, SinonSandbox } from "sinon";
+import getMemberUtil from "../../src/utils/getMemberUtil";
+import {BaseMocks, CustomMocks} from "@lambocreeper/mock-discord.js";
+import {Collection, GuildMember} from "discord.js";
+
+describe("getMemberUtil", () => {
+	describe("::getGuildMember()", () => {
+		let sandbox: SinonSandbox;
+
+		beforeEach(() => {
+			sandbox = createSandbox();
+		});
+
+		it("returns GuildMember if value is a userID string", () => {
+			const guild = CustomMocks.getGuild();
+			const user = CustomMocks.getUser({id: "123456789", username: "fakeUser", discriminator: "12345"});
+
+			guild.addMember(user, {accessToken: "123456789"});
+
+			expect(getMemberUtil.getGuildMember("fakeUser#1234", guild)).to.deep.equal(GuildMember);
+		});
+	});
+});
