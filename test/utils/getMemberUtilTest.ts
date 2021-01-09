f0e1b167e5f181dc0f0ebd5fdc1ab629aeb9140a
BlackBearFTW <robinmager@home.nl>
2021-01-09 10:36:01 +0100

    Completed getMemerUtil Tests

--- test/utils/getMemberUtilTest.ts
+++ test/utils/getMemberUtilTest.ts
@@ -2,7 +2,10 @@ import { expect } from "chai";
 import { createSandbox, SinonSandbox } from "sinon";
 import getMemberUtil from "../../src/utils/getMemberUtil";
 import {BaseMocks, CustomMocks} from "@lambocreeper/mock-discord.js";
-import {Collection, GuildMember, GuildMemberManager} from "discord.js";
+import {Collection, GuildMemberManager} from "discord.js";
+
+const user = CustomMocks.getUser({id: "123456789", username: "fakeUser", discriminator: "1234"});
+const member = CustomMocks.getGuildMember({user: user});
 
 describe("getMemberUtil", () => {
 	describe("::getGuildMember()", () => {
@@ -12,13 +15,27 @@ describe("getMemberUtil", () => {
 			sandbox = createSandbox();
 		});
 
-		it("returns GuildMember if value is a userID string", async () => {
-			const user = CustomMocks.getUser({id: "123456789", username: "fakeUser", discriminator: "12345"});
-			const member = CustomMocks.getGuildMember({user: user});
-
+		it("returns GuildMember if value is a username + discriminator", async () => {
 			sandbox.stub(GuildMemberManager.prototype, "fetch").resolves(new Collection([["12345", member]]));
 
 			expect(await getMemberUtil.getGuildMember("fakeUser#1234", BaseMocks.getGuild())).to.equal(member);
+		});
+
+		it("returns GuildMember if value is a username", async () => {
+			sandbox.stub(GuildMemberManager.prototype, "fetch").resolves(new Collection([["12345", member]]));
+
+			expect(await getMemberUtil.getGuildMember("fakeUser", BaseMocks.getGuild())).to.equal(member);
+		});
+
+		it("returns GuildMember if value is a userID", async () => {
+			// @ts-ignore (the types aren't recognising the overloaded fetch function)
+			sandbox.stub(GuildMemberManager.prototype, "fetch").resolves(member);
+
+			expect(await getMemberUtil.getGuildMember("123456789", BaseMocks.getGuild())).to.equal(member);
+		});
+
+		afterEach(() => {
+			sandbox.restore();
 		});
 	});
 });
