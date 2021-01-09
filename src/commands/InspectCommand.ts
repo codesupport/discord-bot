0bd999e165cad764e158661fd94e3a1b58de14ba
BlackBearFTW <robinmager@home.nl>
2021-01-09 19:01:14 +0100

    Extracted embeds to private methods

--- src/commands/InspectCommand.ts
+++ src/commands/InspectCommand.ts
@@ -1,5 +1,5 @@
-import { GuildMember, Message, MessageEmbed } from "discord.js";
-import getMemberUtil from "../utils/getMemberUtil";
+import {GuildMember, Message, MessageEmbed} from "discord.js";
+import DiscordUtil from "../utils/DiscordUtil";
 import Command from "../abstracts/Command";
 import DateUtils from "../utils/DateUtils";
 import { EMBED_COLOURS } from "../config.json";
@@ -13,49 +13,47 @@ class InspectCommand extends Command {
 	}
 
 	async run(message: Message, args: string[]) {
-		const embed = new MessageEmbed();
+		let embed: MessageEmbed;
 
 		try {
-			let userObj: GuildMember | undefined;
+			const userObj = args.length > 0 ? await DiscordUtil.getGuildMember(args[0], message.guild!) : message.member!;
 
-			if (args.length > 0) {
-				userObj = await getMemberUtil.getGuildMember(args[0], message.guild!);
-			} else {
-				if (message.member === null) return;
-				userObj = message.member;
-			}
-
-			if (userObj !== undefined) {
-				if (message.guild === undefined) return;
-
-				embed.setTitle(`Inspecting ${userObj?.user.tag}`);
-				embed.setThumbnail(userObj?.user.displayAvatarURL());
-				embed.addField("User ID", userObj?.user.id);
-				embed.addField("Username", userObj?.user.tag);
-				if (userObj?.nickname !== null) embed.addField("Nickname", userObj?.nickname);
-				if (userObj?.joinedAt !== null) embed.addField("Joined At", DateUtils.formatAsText(userObj?.joinedAt));
-
-				if (userObj?.roles.cache.size > 1) {
-					embed.addField("Roles", `${userObj.roles.cache.filter(role => role.id !== message?.guild!.id).map(role => ` ${role.toString()}`)}`);
-				} else {
-					embed.addField("Roles", "No roles");
-				}
-
-				embed.setColor(userObj?.displayColor || EMBED_COLOURS.DEFAULT);
-			} else {
-				embed.setTitle("Error");
-				embed.setDescription("No match found.");
-				embed.addField("Correct Usage", "?inspect [username|userID]");
-				embed.setColor(EMBED_COLOURS.ERROR);
-			}
+			embed = userObj === undefined ? this._errorEmbed("No match found.") : this._inspectEmbed(userObj!);
 		} catch (error) {
-			embed.setTitle("Error");
-			embed.setDescription("Incorrect usage of command");
-			embed.addField("Correct Usage", "?inspect [username|userID]");
-			embed.setColor(EMBED_COLOURS.ERROR);
+			embed = this._errorEmbed("Incorrect usage of command.");
 		}
 
 		await message.channel.send({embed});
+	}
+
+	private _errorEmbed(errorMessage: string): MessageEmbed {
+		const embed = new MessageEmbed();
+
+		embed.setTitle("Error");
+		embed.setDescription(errorMessage);
+		embed.addField("Correct Usage", "?inspect [username|userID]");
+		embed.setColor(EMBED_COLOURS.ERROR);
+		return embed;
+	}
+
+	private _inspectEmbed(memberObj: GuildMember): MessageEmbed {
+		const embed = new MessageEmbed();
+
+		embed.setTitle(`Inspecting ${memberObj?.user.tag}`);
+		embed.setColor(memberObj?.displayColor || EMBED_COLOURS.DEFAULT);
+		embed.setThumbnail(memberObj?.user.displayAvatarURL());
+		embed.addField("User ID", memberObj?.user.id);
+		embed.addField("Username", memberObj?.user.tag);
+		if (memberObj?.nickname !== null) embed.addField("Nickname", memberObj?.nickname);
+		embed.addField("Joined At", DateUtils.formatAsText(memberObj?.joinedAt!));
+
+		if (memberObj?.roles.cache.size > 1) {
+			embed.addField("Roles", `${memberObj.roles.cache.filter(role => role.id !== memberObj?.guild!.id).map(role => ` ${role.toString()}`)}`);
+		} else {
+			embed.addField("Roles", "No roles");
+		}
+
+		return embed;
 	}
 }
 
