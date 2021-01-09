c065f3fea6b7c88d8e751e68056598841eb2daa5
BlackBearFTW <robinmager@home.nl>
2021-01-09 18:34:36 +0100

    Made getGuildMember more readable

--- src/utils/DiscordUtil.ts
+++ src/utils/DiscordUtil.ts
@@ -4,25 +4,27 @@ class DiscordUtil {
 	static async getGuildMember(value: string, guild: Guild): Promise<GuildMember | undefined> {
 		if (value === "") return;
 
+		// UserID
 		if ((/^[0-9]+$/g).test(value)) {
-			// UserID
 			try {
 				return await guild.members.fetch(value);
 			} catch (error) {
 				return undefined;
 			}
-		} else if ((/^.*#[0-9]{4}$/g).test(value)) {
-			// Username + discriminator
+		}
+
+		// Username + discriminator
+		if ((/^.*#[0-9]{4}$/g).test(value)) {
 			const [username, discriminator] = value.split("#");
 			const memberList = await guild.members.fetch({query: username});
 
 			return memberList?.find(memberObject => memberObject.user.discriminator === discriminator);
-		} else {
-			// Username without discriminator
-			const guildMember = await guild.members.fetch({query: value});
-
-			return guildMember.first();
 		}
+
+		// Username without discriminator
+		const guildMember = await guild.members.fetch({query: value});
+
+		return guildMember.first();
 	}
 }
 
