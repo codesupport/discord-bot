243ba963c2f3590d0dc6869f539dfa4b8e4fe09d
BlackBearFTW <robinmager@home.nl>
2021-01-09 17:26:40 +0100

    Updated DiscordUtil after Jason's feedback

--- src/utils/DiscordUtil.ts
+++ src/utils/DiscordUtil.ts
@@ -6,18 +6,18 @@ class DiscordUtil {
 
 		if ((/^[0-9]+$/g).test(value)) {
 			// UserID
-			return guild.members?.fetch(value) || undefined;
+			return await guild.members.fetch(value) || undefined;
 		} else if ((/^.*#[0-9]{4}$/g).test(value)) {
 			// Username + discriminator
 			const [username, discriminator] = value.split("#");
-			const memberList = await guild.members?.fetch({query: username});
+			const memberList = await guild.members.fetch({query: username});
 
-			return await memberList?.find(memberObject => memberObject.user.discriminator === discriminator) || undefined;
+			return memberList?.find(memberObject => memberObject.user.discriminator === discriminator);
 		} else {
 			// Username without discriminator
-			const guildMember = await guild.members?.fetch({query: value});
+			const guildMember = await guild.members.fetch({query: value});
 
-			return guildMember.first() || undefined;
+			return guildMember.first();
 		}
 	}
 }
