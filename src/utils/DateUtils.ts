c6e7a91192f7ffe668f5621b76c4b5233f53c2ee
BlackBearFTW <robinmager@home.nl>
2021-01-15 20:37:14 +0100

    Updated DateUtil

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -28,12 +28,15 @@ class DateUtils {
 		let daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
 
 		difference -= daysDifference * 1000 * 60 * 60 * 24;
+
 		const hoursDifference = Math.floor(difference / 1000 / 60 / 60);
 
 		difference -= hoursDifference * 1000 * 60 * 60;
+
 		const minutesDifference = Math.floor(difference / 1000 / 60);
 
 		difference -= minutesDifference * 1000 * 60;
+
 		const secondDifference = Math.floor(difference / 1000);
 		const yearsDifference = Math.floor(daysDifference / 365);
 
