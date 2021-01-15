63bbeb021ada8971785d935c085399da01cf4453
J141 <J.dekoning141@gmail.com>
2021-01-15 21:47:52 +0100

    GhostPingHandler now adds relevant info for deleted replies. (#150)
    * GhostPingHandler now adds relevant info for deleted replies.
    
    * Add unit test for reply info
    
    * add formatting to pipes
    
    * merge 'Author' and 'Reply to' fields into single field
    
    * Remove double null check
    
    * change 'Reply to' to inline field, revert change to unit test

--- src/event/handlers/GhostPingHandler.ts
+++ src/event/handlers/GhostPingHandler.ts
@@ -1,4 +1,4 @@
-import { Constants, MessageEmbed, Message } from "discord.js";
+import { Constants, MessageEmbed, Message, User, TextChannel } from "discord.js";
 import { EMBED_COLOURS } from "../../config.json";
 import DateUtils from "../../utils/DateUtils";
 import EventHandler from "../../abstracts/EventHandler";
@@ -17,9 +17,33 @@ class GhostPingHandler extends EventHandler {
 
 				const embed = new MessageEmbed();
 
+				let repliedToMessage : Message | null | undefined = null;
+				let repliedToUser: User | null | undefined = null;
+
+				if (message.reference?.messageID && message.reference.guildID === message.guild?.id) {
+					const repliedToChannel = message.guild.channels.resolve(message.reference.channelID);
+
+					if (repliedToChannel instanceof TextChannel) {
+						repliedToMessage = await repliedToChannel.messages.fetch(message.reference.messageID);
+						repliedToUser = repliedToMessage?.author;
+					}
+				}
+
 				embed.setTitle("Ghost Ping Detected!");
-				embed.addField("Author", message.author);
+
+				if (repliedToUser !== null && repliedToUser !== undefined) {
+					embed.addField("Author", message.author, true);
+					embed.addField("Reply to", repliedToUser, true);
+				} else {
+					embed.addField("Author", message.author);
+				}
+
 				embed.addField("Message", message.content);
+
+				if (repliedToMessage !== null && repliedToMessage !== undefined && repliedToMessage !== null) {
+					embed.addField("Message replied to", `https://discord.com/channels/${repliedToMessage.guild?.id}/${repliedToMessage.channel.id}/${repliedToMessage.id}`, true);
+				}
+
 				embed.setFooter(`Message sent at ${DateUtils.formatAsText(message.createdAt)}`);
 				embed.setColor(EMBED_COLOURS.DEFAULT);
 
--- test/event/handlers/GhostPingHandlerTest.ts
+++ test/event/handlers/GhostPingHandlerTest.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Constants, MessageMentions } from "discord.js";
+import { Collection, Constants, Guild, Message, MessageEmbed, MessageMentions, MessageReference } from "discord.js";
 import { SinonSandbox, createSandbox } from "sinon";
 import { BaseMocks, CustomMocks } from "@lambocreeper/mock-discord.js";
 
@@ -90,6 +90,46 @@ describe("GhostPingHandler", () => {
 
 			await handler.handle(message);
 			expect(messageMock.called).to.be.true;
+		});
+
+		it("provides additional info if message is a reply to another message", async () => {
+			const message = CustomMocks.getMessage({guild: CustomMocks.getGuild()});
+			const messageMock = sandbox.stub(message.channel, "send");
+			const channelMock = CustomMocks.getTextChannel();
+			const repliedToMessage = CustomMocks.getMessage({	id: "328194044587147280", guild: CustomMocks.getGuild()});
+			const resolveChannelStub = sandbox.stub(message.guild.channels, "resolve").returns(channelMock);
+			const fetchMessageStub = sandbox.stub(channelMock.messages, "fetch").returns(Promise.resolve(repliedToMessage));
+			const author = CustomMocks.getUser();
+
+			message.author = author;
+			message.mentions = new MessageMentions(message, [CustomMocks.getUser({ id: "328194044587147278" })], [], false);
+			message.guild.id = "328194044587147279";
+			message.content = "this is a reply";
+			message.reference = {
+				channelID: "328194044587147278",
+				guildID: "328194044587147279",
+				messageID: "328194044587147280"
+			};
+
+			repliedToMessage.channel = CustomMocks.getTextChannel({ id: "328194044587147278"});
+
+			await handler.handle(message);
+			expect(messageMock.called).to.be.true;
+			expect(resolveChannelStub.called).to.be.true;
+			expect(fetchMessageStub.called).to.be.true;
+			const sentEmbed = messageMock.getCall(0).args[0];
+
+			expect(sentEmbed).to.be.an.instanceOf(MessageEmbed);
+			if (sentEmbed instanceof MessageEmbed) {
+				const replyToField = sentEmbed.fields.find(field => field.name === "Reply to");
+
+				expect(replyToField).to.not.be.null;
+
+				const messageLinkField = sentEmbed.fields.find(field => field.name === "Message replied to");
+
+				expect(messageLinkField).to.not.be.null;
+				expect(messageLinkField.value).to.equal("https://discord.com/channels/328194044587147279/328194044587147278/328194044587147280");
+			}
 		});
 
 		afterEach(() => {
