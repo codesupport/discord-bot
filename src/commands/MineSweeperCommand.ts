06752427282d7ea9f199bd1f94f07a7237069a26
BlackBearFTW <robinmager@home.nl>
2021-05-22 14:16:34 +0200

    added difficulty options

--- src/commands/MineSweeperCommand.ts
+++ src/commands/MineSweeperCommand.ts
@@ -15,9 +15,30 @@ class MineSweeperCommand extends Command {
 		const embed = new MessageEmbed();
 		const mineSweeperService = MineSweeperService.getInstance();
 
-		embed.setTitle("MineSweeper");
-		embed.setDescription(mineSweeperService.generateGame(9, 9, 5));
-		embed.setColor(EMBED_COLOURS.DEFAULT);
+		if (!args[0]) args[0] = "easy";
+
+		interface DifficultyLevel {
+			[key: string]: {display: string, ratio: number}
+		}
+
+		const difficultyLevels: DifficultyLevel = {
+			"easy": {display: "Easy", ratio: 7},
+			"medium": {display: "Medium", ratio: 5},
+			"hard": {display: "Hard", ratio: 3}
+		};
+
+		if (Object.keys(difficultyLevels).includes(args[0].toLowerCase())) {
+			const difficulty = difficultyLevels[args[0].toLowerCase()];
+
+			embed.setTitle(`MineSweeper (${difficulty.display})`);
+			embed.setDescription(mineSweeperService.generateGame(11, 11, difficulty.ratio));
+			embed.setColor(EMBED_COLOURS.DEFAULT);
+		} else {
+			embed.setTitle("Error");
+			embed.setDescription("You must provide an existing difficulty");
+			embed.addField("Correct Usage", "?minesweeper [easy|medium|hard]");
+			embed.setColor(EMBED_COLOURS.ERROR);
+		}
 
 		await message.channel.send({ embed });
 	}
