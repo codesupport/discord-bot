b6dad9ea0b935a97c49e0efbc3629572c26e080a
BlackBearFTW <robinmager@home.nl>
2021-05-23 13:01:42 +0200

    Refactoring MineSweeperService

--- src/services/MineSweeperService.ts
+++ src/services/MineSweeperService.ts
@@ -4,6 +4,7 @@ class MineSweeperService {
 	private static instance: MineSweeperService;
 	private static readonly GRID_ROWS = 11;
 	private static readonly GRID_COLUMNS = 11;
+	private static readonly BOMB = ":boom:";
 
 	/* eslint-disable */
 	private constructor() { }
@@ -19,35 +20,50 @@ class MineSweeperService {
 
 	public generateGame(ratio: number): string {
 		const bombCount = MineSweeperService.GRID_ROWS * MineSweeperService.GRID_COLUMNS / ratio;
-		const bombIndex = [];
-		const numbers = [":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:"];
-		const bombSurroundingPositions = [[0, -1], [0, 1], [-1, -1], [-1, 0], [-1, 1], [1, -1], [1, 0], [1, 1]];
-		const bomb = ":boom:";
+		const emptyGrid = Array.from(Array(MineSweeperService.GRID_ROWS), () => new Array(MineSweeperService.GRID_COLUMNS).fill(0));
 
-		const grid = Array.from(Array(MineSweeperService.GRID_ROWS), () => new Array(MineSweeperService.GRID_COLUMNS).fill(0));
+		const [gridWithBombs, bombPositions] = this.placeBombs(emptyGrid, bombCount);
 
-		let currentCount = 0;
+		const gameGrid = this.calculateBombSurrounding(gridWithBombs, bombPositions);
 
-		while (currentCount < bombCount) {
+		return this.formatGameResult(gameGrid);
+	}
+
+	private placeBombs(grid: any[][], bombCount: number) {
+		const bombPositions = [];
+
+		for (let i = 0; i < bombCount; i++) {
 			const randomRow = NumberUtils.getRandomNumberInRange(0, MineSweeperService.GRID_ROWS - 1);
 			const randomColumn = NumberUtils.getRandomNumberInRange(0, MineSweeperService.GRID_COLUMNS - 1);
 
 			// eslint-disable-next-line no-continue
-			if (grid[randomRow][randomColumn] === bomb) continue;
-			grid[randomRow][randomColumn] = bomb;
-			bombIndex.push([randomRow, randomColumn]);
-			currentCount++;
+			if (grid[randomRow][randomColumn] === MineSweeperService.BOMB) continue;
+			grid[randomRow][randomColumn] = MineSweeperService.BOMB;
+			bombPositions.push([randomRow, randomColumn]);
 		}
 
-		bombIndex.map(([rowI, colI]) => {
+		return [grid, bombPositions];
+	}
+
+	private calculateBombSurrounding(grid: any[][], bombPositions: any[][]) {
+		const bombSurroundingPositions = [[0, -1], [0, 1], [-1, -1], [-1, 0], [-1, 1], [1, -1], [1, 0], [1, 1]];
+
+		bombPositions.map(([rowI, colI]) => {
 			// Check around every bomb and add +1 to cell counter
 			bombSurroundingPositions.map(([rowPosition, colPosition]) => {
-				if (this.isOutsideEdges(rowI + rowPosition, colI + colPosition)) return;
+				if (typeof grid[rowI + rowPosition] === "undefined") return;
 				if (typeof grid[rowI + rowPosition][colI + colPosition] !== "number") return;
 
 				grid[rowI + rowPosition][colI + colPosition] += 1;
 			});
 		});
+
+		return grid;
+	}
+
+	private formatGameResult(grid: any[][]) {
+		const numbers = [":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:"];
+		let stringGrid = "";
 
 		for (let rowI = 0; rowI < MineSweeperService.GRID_ROWS; rowI++) {
 			for (let colI = 0; colI < MineSweeperService.GRID_COLUMNS; colI++) {
@@ -57,22 +73,11 @@ class MineSweeperService {
 			}
 		}
 
-		let stringGrid = "";
-
 		grid.map((row, index) => {
 			stringGrid += `||${grid[index].join("||||")}||\n`;
 		});
 
 		return stringGrid;
-	}
-
-	private isOutsideEdges(rowIndex: number, colIndex: number) {
-		// Top/Bottom
-		if (rowIndex < 0 || rowIndex === MineSweeperService.GRID_ROWS) return true;
-		// Sides
-		if (colIndex < 0 || colIndex === MineSweeperService.GRID_COLUMNS) return true;
-
-		return false;
 	}
 }
 
