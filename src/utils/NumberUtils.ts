69421d3dcc8c3c56f3ff244432dcc29988037e78
Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>
2021-06-06 13:53:52 +0200

    Revert "Added a minesweeper command. (#173)"
    This reverts commit 4d50baf2da19079ff9227477878bcc6d8b05c382.

--- src/utils/NumberUtils.ts
+++
@@ -1,7 +0,0 @@
-class NumberUtils {
-	static getRandomNumberInRange(min: number, max: number): number {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-}
-
-export default NumberUtils;
