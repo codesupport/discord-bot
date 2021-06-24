a7bd2b98c130ad07924e791a2eefc0fdbf3f67c1
BlackBearFTW <robinmager@home.nl>
2021-06-24 17:58:23 +0200

    Refactored RaidDetection and added option to slow down warning amount

--- src/event/handlers/RaidDetectionHandler.ts
+++ src/event/handlers/RaidDetectionHandler.ts
@@ -1,53 +1,61 @@
 import {Constants, GuildMember, MessageEmbed, TextChannel} from "discord.js";
 import EventHandler from "../../abstracts/EventHandler";
-import { RAID_SETTINGS, LOG_CHANNEL_ID, GENERAL_CHANNEL_ID, MOD_ROLE, EMBED_COLOURS } from "../../config.json";
+import {EMBED_COLOURS, GENERAL_CHANNEL_ID, LOG_CHANNEL_ID, MOD_ROLE, RAID_SETTINGS} from "../../config.json";
 import getConfigValue from "../../utils/getConfigValue";
 
 class RaidDetectionHandler extends EventHandler {
 	private joinQueue: GuildMember[] = [];
+	private cycleCount = 0;
 
 	constructor() {
 		super(Constants.Events.GUILD_MEMBER_ADD);
 	}
 
-	handle = async (member: GuildMember): Promise<void> => {
+	async handle(member: GuildMember): Promise<void> {
 		const timeToWait = 1000 * getConfigValue<number>("RAID_SETTINGS.TIME_TILL_REMOVAL");
 		const modChannel = member.guild?.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID) as TextChannel;
 		const generalChannel = member.guild?.channels.cache.find(channel => channel.id === GENERAL_CHANNEL_ID) as TextChannel;
 
 		this.joinQueue.push(member);
 
-		if (this.joinQueue.length >= RAID_SETTINGS.MAX_QUEUE_SIZE) {
-			try {
-				await Promise.all(this.joinQueue.map(async member => {
-					await member.kick("Detected as part of a raid.");
-					await modChannel.send(`@<${MOD_ROLE}> **RAID DETECTION** Kicked user ${member.displayName} (${member.id}).`);
-				}));
-
-				this.joinQueue = [];
-
-				const embed = new MessageEmbed();
-
-				embed.setTitle(":warning: Raid Detected");
-				embed.setDescription(`**We have detected a raid is currently going on and are solving the issue.**
-					Please refrain from notifying the moderators or spamming this channel.
-					Thank you for your cooperation and we apologise for any inconvenience.`);
-				embed.setColor(EMBED_COLOURS.WARNING);
-				embed.setTimestamp();
-
-				await generalChannel.send(embed);
-			} catch (error) {
-				console.error(error);
-
-				await modChannel.send(`Failed to kick users or empty queue: \n\`${error.message}\``);
-			}
+		if (this.joinQueue.length < RAID_SETTINGS.MAX_QUEUE_SIZE) {
+			setTimeout(() => this.joinQueue.shift(), timeToWait);
+			return;
 		}
 
-		setTimeout(() => {
-			if (this.joinQueue.includes(member)) {
-				this.joinQueue.splice(this.joinQueue.indexOf(member), 1);
+		try {
+			for (const member of this.joinQueue) {
+				await member.kick("Detected as part of a raid.");
+				await modChannel.send(`@<${MOD_ROLE}> **RAID DETECTION** Kicked user ${member.displayName} (${member.id}).`);
 			}
-		}, timeToWait);
+
+			this.joinQueue = [];
+
+			if (this.cycleCount < RAID_SETTINGS.WARN_CYCLE_COUNT) {
+				this.cycleCount++;
+				return;
+			}
+
+			await generalChannel.send(this.buildRaidEmbed());
+			this.cycleCount = 0;
+		} catch (error) {
+			console.error(error);
+
+			await modChannel.send(`Failed to kick users or empty queue: \n\`${error.message}\``);
+		}
+	}
+
+	private buildRaidEmbed(): MessageEmbed {
+		const embed = new MessageEmbed();
+
+		embed.setTitle(":warning: Raid Detected");
+		embed.setDescription(`**We have detected a raid is currently going on and are solving the issue.**
+					Please refrain from notifying the moderators or spamming this channel.
+					Thank you for your cooperation and we apologise for any inconvenience.`);
+		embed.setColor(EMBED_COLOURS.WARNING);
+		embed.setTimestamp();
+
+		return embed;
 	}
 }
 
