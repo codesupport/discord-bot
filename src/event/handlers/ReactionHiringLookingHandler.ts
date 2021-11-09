39e28b2479fe20fd6d0f99fa2732f57cd6e74dc9
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2021-11-09 23:29:37 +0100

    Downgraded discord.js + fixed linting

--- src/event/handlers/ReactionHiringLookingHandler.ts
+++ src/event/handlers/ReactionHiringLookingHandler.ts
@@ -1,22 +1,23 @@
-import EventHandler from '../../abstracts/EventHandler';
-import { Constants, MessageReaction, User } from 'discord.js';
-import { BOTLESS_CHANNELS } from '../../config.json';
+import EventHandler from "../../abstracts/EventHandler";
+import { Constants, MessageReaction, User } from "discord.js";
+import { BOTLESS_CHANNELS } from "../../config.json";
 
 class ReactionHiringLookingHandler extends EventHandler {
-  constructor() {
-    super(Constants.Events.MESSAGE_REACTION_ADD);
-  }
+	constructor() {
+		super(Constants.Events.MESSAGE_REACTION_ADD);
+	}
 
-  async handle(reaction: MessageReaction, member: User): Promise<void> {
-    const { message } = reaction;
-    const { channel } = message;
+	async handle(reaction: MessageReaction, member: User): Promise<void> {
+		const { message } = reaction;
+		const { channel } = message;
 
-    const isHLChannel = channel.id == BOTLESS_CHANNELS.HIRING_OR_LOOKING;
-    const isSelfReact = message.author?.id == member.id
-    if (isHLChannel && isSelfReact) {
-      reaction.users.remove(member);
-    }
-  }
+		const isHLChannel = channel.id === BOTLESS_CHANNELS.HIRING_OR_LOOKING;
+		const isSelfReact = message.author?.id === member.id;
+
+		if (isHLChannel && isSelfReact) {
+			reaction.users.remove(member);
+		}
+	}
 }
 
 export default ReactionHiringLookingHandler;
