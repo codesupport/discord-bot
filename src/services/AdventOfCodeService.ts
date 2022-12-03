6bfba923744eace5dadc45e774f787b30c60015c
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-12-03 21:39:16 +0000

    Add missing comma

--- src/services/AdventOfCodeService.ts
+++ src/services/AdventOfCodeService.ts
@@ -24,7 +24,7 @@ export default class AdventOfCodeService {
 
 		const response = await this.api.get<AOCLeaderBoard>(link, {
 			headers: {
-				"Cookie": `session=${ADVENT_OF_CODE_TOKEN};`
+				"Cookie": `session=${ADVENT_OF_CODE_TOKEN};`,
 				"User-Agent": `github.com/codesupport/discord-bot by ${process.env.CONTACT_EMAIL}`
 			}
 		});
