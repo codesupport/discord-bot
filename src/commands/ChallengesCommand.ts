f06777c0ae7db486903bb12e6c7c723676e9b88d
hexa6 <hexathe6@gmail.com>
2024-02-05 14:10:16 -0600

    hopefully this fixed it for real this time

--- src/commands/ChallengesCommand.ts
+++ src/commands/ChallengesCommand.ts
@@ -5,18 +5,18 @@ import GenericObject from "../interfaces/GenericObject";
 
 @Discord()
 class ChallengesCommand {
-  @Slash({ name: "challenges", description: "Shows a list of programming challenges" })
-  async onInteract(interaction: CommandInteraction): Promise<void> {
-    const embed = new EmbedBuilder();
-    const image = new AttachmentBuilder("./assets/programming_challenges_v4.0.png", { name: "programming_challenges_v4.0.png" });
-    
-    embed.setTitle("Programming Challenges");
-    embed.setDescription("Try some of these!");
-    embed.setImage("attachment://programming_challenges_v4.0.png");
-    embed.setColor(getConfigValue<GenericObject<ColorResolvable>>("EMBED_COLOURS").DEFAULT);
-    
-    await interaction.reply({ embeds: [embed], files: [image] });
-  }
+	@Slash({ name: "challenges", description: "Shows a list of programming challenges" })
+	async onInteract(interaction: CommandInteraction): Promise<void> {
+		const embed = new EmbedBuilder();
+		const image = new AttachmentBuilder("./assets/programming_challenges_v4.0.png", { name: "programming_challenges_v4.0.png" });
+
+		embed.setTitle("Programming Challenges");
+		embed.setDescription("Try some of these!");
+		embed.setImage("attachment://programming_challenges_v4.0.png");
+		embed.setColor(getConfigValue<GenericObject<ColorResolvable>>("EMBED_COLOURS").DEFAULT);
+
+		await interaction.reply({ embeds: [embed], files: [image] });
+	}
 }
 
-export default CodeblockCommand;
+export default ChallengesCommand;
